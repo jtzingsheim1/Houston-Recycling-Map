14b319597008c1d680757bba8b4a4dd424d1a2d5
jtzingsheim1 <43869157+jtzingsheim1@users.noreply.github.com>
2019-06-04 17:42:28 -0500

    Created and started on the script file

---
+++ C9_Project1.R
@@ -0,0 +1,52 @@
+# Coursera Data Science Specialization Course 9 Project 1 Script----------------
+# Create a Webpage that Features a Map Created with Leaflet
+
+
+# Acknowledgement for this dataset goes to the City of Houston who make this
+# data available through their GIS open data portal which is located at the
+# URL: https://cohgis-mycity.opendata.arcgis.com/
+
+
+# The purpose of this script is to complete the basic requirements behind the
+# project 1 peer-graded assignment which is part of the Developing Data Products
+# course from Johns Hopkins University within the Data Science Specialization on
+# Coursera.
+#
+# The instructions say:
+# "Create a web page using R Markdown that features a map created with Leaflet.
+# Host your webpage on either GitHub Pages, RPubs, or NeoCities. Your webpage
+# must contain the date that you created the document, and it must contain a map
+# created with Leaflet. We would love to see you show off your creativity!
+#
+# The input for this document is the COHGIS Recycling Center Locations dataset
+# which comes from the URL below. The script leaves behind a data objects, but
+# the primary purpose is completing the core of the project, so end deliverables
+# can be easily created later.
+
+
+#library(tidyverse)
+#library(leaflet)
+
+
+# Part 0) Function definitions--------------------------------------------------
+
+
+# Part 1) Loading and preprocessing the data------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
